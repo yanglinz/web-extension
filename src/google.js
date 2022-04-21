0e40b984f84912b0dbba889a7bbcd23304129194
Yanglin Zhao <yz1876@gmail.com>
2022-04-20 23:14:10 -0400

    Ignore w3schools link in google

--- src/google.js
+++ src/google.js
@@ -1,1 +1,23 @@
-console.log("Hello world!");
+function ready(fn) {
+  if (document.readyState != "loading") {
+    fn();
+  } else {
+    document.addEventListener("DOMContentLoaded", fn);
+  }
+}
+
+const IGNORE_LIST = ["www.w3schools.com"];
+
+function hideLinksByDomains(domains = IGNORE_LIST) {
+  domains.forEach((d) => {
+    const ignoredLinks = document.querySelectorAll(`a[href*="${d}"]`);
+    if (ignoredLinks) {
+      ignoredLinks.forEach((l) => {
+        const container = l.parentNode.parentNode.parentNode;
+        container.style.opacity = "0.1";
+      });
+    }
+  });
+}
+
+ready(hideLinksByDomains);
