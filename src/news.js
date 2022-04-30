eaacd78c4a3638547fe5046b14823524ea2da79c
Yanglin Zhao <hi@yanglinzhao.com>
2022-04-29 22:23:18 -0400

    Add archive link

--- src/news.js
+++ src/news.js
@@ -1,7 +1,19 @@
 import { onReady } from "./lib/dom";
 
+import "./news.css";
+
+function getArchiveLink(currentLink = document.location.href) {
+  // TODO: Get dynamic date url
+  return `https://web.archive.org/web/20220429171756/${currentLink}`;
+}
+
 function showArchiveLink() {
-  console.log("Hello world!");
+  const el = document.createElement("div");
+  el.className = "InjectedInternetArchiveLink";
+  el.innerHTML = `
+    <a href="${getArchiveLink()}">${getArchiveLink()}</a>
+  `;
+  document.body.appendChild(el);
 }
 
 onReady(showArchiveLink);
