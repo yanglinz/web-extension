26b3b955c807f2265c63c9e9ae192635a794c9e0
Yanglin Zhao <hi@yanglinzhao.com>
2022-04-29 22:28:56 -0400

    Progress

--- src/news.js
+++ src/news.js
@@ -3,15 +3,15 @@ import { onReady } from "./lib/dom";
 import "./news.css";
 
 function getArchiveLink(currentLink = document.location.href) {
-  // TODO: Get dynamic date url
-  return `https://web.archive.org/web/20220429171756/${currentLink}`;
+  const futureDate = "20300101000000";
+  return `https://web.archive.org/web/${futureDate}/${currentLink}`;
 }
 
 function showArchiveLink() {
   const el = document.createElement("div");
   el.className = "InjectedInternetArchiveLink";
   el.innerHTML = `
-    <a href="${getArchiveLink()}">${getArchiveLink()}</a>
+    <a href="${getArchiveLink()}">web.archive.org</a>
   `;
   document.body.appendChild(el);
 }
